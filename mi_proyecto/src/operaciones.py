b7c7b701bd05ebc00d5bcd7995a89c43c2ad2809
pepese3 <adri.anmoyanocorreo@gmail.com>
2026-04-25 22:14:50 +0000

    Primer commit

---
+++ mi_proyecto/src/operaciones.py
@@ -0,0 +1,3 @@
+def filtrar_pares(lista: list) -> list:
+                   'Recibe una lista de numeros y devuelve solo los numeros pares'
+                   return [i for i in lista if i % 2 == 0]
